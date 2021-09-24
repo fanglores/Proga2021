fa892972b77b01fb6ab8ea12d53c302a98e4812a
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-09-24 21:16:33 +0300

    Update Task5.cpp

--- Task 5 (Lecture 4)/Task5.cpp
+++ Task 5 (Lecture 4)/Task5.cpp
@@ -52,17 +52,29 @@ public:
 
     int get(int x, int y) 
     {
-        if (x > mx || y > my || x < 0 || y < 0) throw 404;
+        if (x >= mx || y >= my || x < 0 || y < 0) throw 404;
         return img[y * mx + x];
     }
 
     int set(int x, int y, int color) 
     { 
-        if(x > mx || y > my || x < 0 || y < 0) throw 404;
+        if(x >= mx || y >= my || x < 0 || y < 0) throw 404;
         img[y * mx + x] = color;
     }
 
     friend ostream& operator<<(ostream&, const Image&);     // Реализовать  для варианта 2
+    Image& operator=(const Image& im)
+    {
+        if (&im == this) return *this;
+
+        mx = im.mx;
+        my = im.my;
+        img = new int[mx * my];
+
+        for (int i = 0; i < mx * my; i++) img[i] = im.img[i];
+
+        return *this;
+    }
 };
 
 ostream& operator<<(ostream& out, const Image& d1)
@@ -89,7 +101,11 @@ int main()
 
         cout << i << endl;
 
-        i.get(3, 2);
+        Image i1(0, 0); i1 = i;
+        cout << i1 << endl;
+
+        cout << i.get(0, 1) << endl;
+        cout << i.get(-1, 0);
     }
     catch (int e)
     {
