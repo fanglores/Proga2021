8f80b5813bf55b5754dca2a416657bc49c21d99f
Konstantin <72685417+fanglores@users.noreply.github.com>
2021-11-18 11:54:02 +0300

    Update Task8.cpp

--- Task 8 (Lecture 7)/Task8.cpp
+++ Task 8 (Lecture 7)/Task8.cpp
@@ -22,13 +22,18 @@ using namespace std;
 */
 
 template<class K, class V>
-struct HashRow
+struct HashNode
 {
 	K key;
 	V value;
 
-	HashRow() = delete;
-	HashRow(K key, V value)
+	HashNode()
+	{
+		key = -1;
+		value = -1;
+	}
+
+	HashNode(K key, V value)
 	{
 		this->key = key;
 		this->value = value;
@@ -36,30 +41,120 @@ struct HashRow
 
 };
 
-template<class V>
+template<class K, class V>
+struct HashRow
+{
+	int size;
+	HashNode<K, V>* row;
+
+	HashRow()
+	{
+		size = 0;
+	}
+
+	HashRow(HashNode<K,V> node)
+	{
+		size = 1;
+		row = new HashNode<K, V>(node.key, node.value);
+	}
+
+	push_back(HashNode<K, V> node)
+	{
+		HashNode<K,V>* trow = new HashNode<K, V>[size + 1];
+		memcpy(trow, row, size * sizeof(HashNode<K, V>));
+		trow[size] = node;
+
+		row = new HashNode<K, V>[size + 1];
+		
+		memcpy(row, trow, (size + 1) * sizeof(HashNode<K,V>));
+		size++;
+	}
+
+	~HashRow()
+	{
+		delete[] row;
+	}
+};
+
+template<class K, class V>
 class HashTable
 {
 private:
+	HashRow<K, V>* table;
 	int size = 0;
-
+	const int k = 7, mod = 13;
 
 public:
 
-	void encrypt(V value)
+	~HashTable()
 	{
+		delete[] table;
+	}
 
+	K encrypt(V value)
+	{
+		int sum = 0;
+		if (typeid(value) == typeid(string))
+		{
+			for (int i = 0; i < value.size(); i++)
+				sum = sum * k + value[i];
+		}
+		else if (typeid(value) == typeid(char) || typeid(value) == typeid(int))
+		{
+			sum = value * k;
+		}
+		else cout << "[RE] Unsupported data type." << endl;
 
+		return (sum % mod);
 	}
 
 	void insert(V value)
 	{
+		HashNode<K, V> temp(encrypt(value), value);
+
+		int i;
+		for (i = 0; i < size; i++) 
+			if (table[i].row[0].key == temp.key) break;
 		
+		if (i == size)
+		{
+			HashRow<K, V>* ttab = new HashRow<K,V>[size + 1];
+			memcpy(ttab, table, size * sizeof(HashRow<K,V>));
+			ttab[size] = temp;
+			
+			table = new HashRow<K,V>[size + 1];
+
+			memcpy(table, ttab, (size + 1) * sizeof(HashRow<K,V>));
+			size++;
+		}
+		else
+		{
+			table[i].push_back(temp);
+		}
 	}
 
-	void find() {}
+	V get(K key) 
+	{
+		
+	
+	}
+
 	void update() {}
 	void remove() {}
 
+
+	void print()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << table[i].row[0].key << '\t';
+			for (int j = 0; j < table[i].size; j++)
+			{
+				cout << table[i].row[j].value << ' ';
+			}
+			cout << endl;
+		}
+	}
 };
 
 void file_processor()
@@ -71,7 +166,10 @@ void file_processor()
 
 int main()
 {
+	HashTable<int, string> ht;
+	ht.insert("abc");
+	ht.insert("bcd");
+	ht.insert("cde");
 
-
-
+	ht.print();
 }
